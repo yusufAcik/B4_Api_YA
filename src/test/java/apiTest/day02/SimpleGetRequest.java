bca36a2946d75f23666666e1019cada436226596
yusufAcik <12yusufacik@gmail.com>
2023-08-22 00:11:43 +0300

    22 8

--- src/test/java/apiTest/day02/SimpleGetRequest.java
+++ src/test/java/apiTest/day02/SimpleGetRequest.java
@@ -1,2 +1,67 @@
-package apiTest.day02;public class SimpleGetRequest {
+package apiTest.day02;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SimpleGetRequest {
+
+    String petStoreURL="https://petstore.swagger.io/v2";
+    String bookCartURL ="https://bookcart.azurewebsites.net";
+    @Test
+    public void basicGetRequest(){
+        Response response = RestAssured.get(petStoreURL+"/store/inventory");
+        //print status code
+        System.out.println("response.statusCode() = " + response.statusCode());
+        //print body
+        System.out.println("response.prettyPrint() = " + response.prettyPrint());
+    }
+
+    @Test
+    public void getRequestWithHeader(){
+        Response response = RestAssured.given()
+                .accept(ContentType.JSON)
+                .when()
+                .get(petStoreURL + "/store/inventory");
+        //response.prettyPrint();
+        Assert.assertEquals(response.statusCode(),200);
+        Assert.assertEquals(response.contentType(),"application/json");
+    }
+    @Test
+    public void getRequest_VerificationWithRestAssured(){
+        //Verify test case with using RestAssured Library
+        RestAssured.given()
+                .accept(ContentType.JSON)
+                .when()
+                .get(petStoreURL + "/store/inventory")
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .and()
+                .contentType("application/json");
+    }
+    @Test
+    public void getRequestWithContainsMethod(){
+        Response response = RestAssured.given()
+                .accept(ContentType.JSON)
+                .when()
+                .get(petStoreURL + "/store/inventory");
+        Assert.assertEquals(response.statusCode(),200);
+        System.out.println("response.body().asString() = " + response.body().asString());
+        Assert.assertTrue(response.body().asString().contains("available"));
+    }
+
+    @Test
+    public void getRequestWithContainsMethod2(){
+        Response response = RestAssured.given()
+                .accept(ContentType.JSON)
+                .when()
+                .get(bookCartURL + "/api/Book");
+        System.out.println("response.body().asString() = " + response.body().asString());
+        Assert.assertEquals(response.statusCode(),200);
+        Assert.assertEquals(response.contentType(),"application/json; charset=utf-8");
+        Assert.assertTrue(response.body().asString().contains("Jonathan Maberry"));
+    }
 }
